2783bb75eae27eb4e01a0e897f5794dec8eba2ca
gunateja5465 <gunatejapattoori1@gmail.com>
2022-01-29 16:42:56 +0530

    final

--- hdimageconverter/settings.py
+++ hdimageconverter/settings.py
@@ -125,8 +125,7 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-STATC_ROOT=os.path.join(BASE_DIR,'static')
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
